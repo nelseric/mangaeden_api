3059c83379b833d2af5d96e9a693f5ada8f1402f
Luca Ferri <luca.ferri88@gmail.com>
2015-01-15 20:12:18 +0100

    Drop useless code

--- lib/mangaeden_api.rb
+++ lib/mangaeden_api.rb
@@ -3,14 +3,3 @@ require 'mangaeden_api/configuration'
 require 'mangaeden_api/mangaeden'
 require 'mangaeden_api/manga'
 require 'mangaeden_api/chapter'
-
-# MangaedenApi module
-module MangaedenApi
-  def self.setup
-    yield configuration
-  end
-
-  def self.configuration
-    @config ||= MangaedenApi::Configuration.instance
-  end
-end
